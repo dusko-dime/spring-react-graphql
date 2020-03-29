f6fc81a7522ffb5f5c24961bbb2dd5dda616cfd3
Duško Dimitrić <dusko.dime@gmail.com>
2020-03-29 19:32:13 +0200

    Added multiple schema files example in spring app.

--- demo/src/main/java/graphql/demo/resolvers/AMutation.java
+++ demo/src/main/java/graphql/demo/resolvers/AMutation.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class VehicleMutation implements GraphQLMutationResolver {
+public class AMutation implements GraphQLMutationResolver {
 
     @Autowired
     private VehicleService vehicleService;
--- demo/src/main/java/graphql/demo/resolvers/BQuery.java
+++ demo/src/main/java/graphql/demo/resolvers/BQuery.java
@@ -7,19 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
 @Component
-public class VehicleQuery implements GraphQLQueryResolver {
+public class BQuery implements GraphQLQueryResolver {
 
     @Autowired
     private VehicleService vehicleService;
 
-    public List<Vehicle> getVehicles(final int count) {
+    public List<Vehicle> getVehiclesFromSecondSchemaFile(final int count) {
         return this.vehicleService.getAllVehicles(count);
     }
 
-    public Optional<Vehicle> getVehicle(final int id) {
-        return this.vehicleService.getVehicle(id);
-    }
 }
